801249c85cde2d5f5c75d0e4a5c0cec7720ccbce
Nate <89227631+0xNeit@users.noreply.github.com>
2024-07-06 00:44:19 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { WalletRadar } from '@razorlabs/m1-wallet-sdk'
+
+function App() {
+  const [connected, setConnected] = useState(false)
+
+  const walletRadar = new WalletRadar();
+  walletRadar.activate();
+
+  const adapters = walletRadar.getDetectedWalletAdapters();
+  const adapter = adapters[0];
+
+  const handleConnect = async () => {
+    await adapter?.connect();
+    console.log(adapter)
+    setConnected(true);
+  }
+
+  const handleDisconnect = async () => {
+    await adapter?.disconnect();
+    setConnected(false);
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        {!connected ? (
+          <button onClick={() => handleConnect()}>
+            Connect Wallet
+          </button>
+        ) : (
+          <button onClick={() => handleDisconnect()}>
+            {adapter.accounts[0]?.address}
+          </button>
+        )}
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
