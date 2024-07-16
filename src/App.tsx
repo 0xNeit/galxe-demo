94acbd756c865152a3a2b89e411fe1566d7c4985
Nate <89227631+0xNeit@users.noreply.github.com>
2024-07-16 17:00:40 +0100

    update sdk version

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { WalletRadar } from '@razorlabs/m1-wallet-sdk'
+import { InputEntryFunctionData, InputGenerateTransactionPayloadData } from '@aptos-labs/ts-sdk'
 
 function App() {
   const [connected, setConnected] = useState(false)
@@ -14,6 +15,12 @@ function App() {
   const adapters = walletRadar.getDetectedWalletAdapters();
   const adapter = adapters.find((a) => a.name === 'Razor Wallet');
 
+  const payload: InputEntryFunctionData = {
+    functionArguments: [false],
+    function: '0x1::aptos_account::set_allow_direct_coin_transfers',
+    typeArguments: [],
+  };
+
   const handleConnect = async () => {
     await adapter?.connect();
     setConnected(true);
@@ -22,6 +29,14 @@ function App() {
   const handleDisconnect = async () => {
     await adapter?.disconnect();
     setConnected(false);
+  }
+
+  const handleTx = async () => {
+    try {
+      adapter?.signAndSubmitTransaction({ payload: payload as InputGenerateTransactionPayloadData });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
@@ -41,9 +56,15 @@ function App() {
             Connect Wallet
           </button>
         ) : (
+          <>
           <button onClick={() => handleDisconnect()}>
             {adapter?.accounts[0]?.address}
           </button>
+
+          <button onClick={async () => handleTx()}>
+            Sign Transaction
+          </button>
+          </>
         )}
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
